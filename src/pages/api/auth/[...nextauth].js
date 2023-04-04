92a76d0b665e6818369d94475dd12566130d0443
0darkace1 <0darkace1@gmail.com>
2023-04-04 22:08:08 +0200

    Moved email function to lib

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,63 +2,9 @@ import NextAuth from "next-auth";
 import EmailProvider from "next-auth/providers/email";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import nodemailer from "nodemailer";
-import Handlebars from "handlebars";
-import { readFileSync } from "fs";
-import path from "path";
 
 import { prisma } from "@/lib/prisma";
-
-const emailsDir = path.resolve(process.cwd(), "emails");
-
-const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_SERVER_HOST,
-  port: process.env.EMAIL_SERVER_PORT,
-  auth: {
-    user: process.env.EMAIL_SERVER_USER,
-    pass: process.env.EMAIL_SERVER_PASSWORD,
-  },
-  secure: false,
-});
-
-const sendVerificationRequest = ({ identifier, url }) => {
-  const emailFile = readFileSync(path.join(emailsDir, "confirm-email.html"), {
-    encoding: "utf8",
-  });
-  const emailTemplate = Handlebars.compile(emailFile);
-  transporter.sendMail({
-    from: `"✨ SupaVacation" ${process.env.EMAIL_FROM}`,
-    to: identifier,
-    subject: "Your sign-in link for SupaVacation",
-    html: emailTemplate({
-      base_url: process.env.NEXTAUTH_URL,
-      signin_url: url,
-      email: identifier,
-    }),
-  });
-};
-
-const sendWelcomeEmail = async ({ user }) => {
-  const { email } = user;
-
-  try {
-    const emailFile = readFileSync(path.join(emailsDir, "welcome.html"), {
-      encoding: "utf8",
-    });
-    const emailTemplate = Handlebars.compile(emailFile);
-    await transporter.sendMail({
-      from: `"✨ SupaVacation" ${process.env.EMAIL_FROM}`,
-      to: email,
-      subject: "Welcome to SupaVacation! 🎉",
-      html: emailTemplate({
-        base_url: process.env.NEXTAUTH_URL,
-        support_email: "support@themodern.dev",
-      }),
-    });
-  } catch (error) {
-    console.log(`❌ Unable to send welcome email to user (${email})`);
-  }
-};
+import { sendWelcomeEmail, sendVerificationRequest } from "@/lib/email";
 
 export default NextAuth({
   pages: {
